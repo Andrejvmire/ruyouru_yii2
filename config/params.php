7aef39c488468c3fca7f3de1260e6db34513e0b2
andrey <andrej_vmire@mail.ru>
2022-04-10 19:00:21 +0300

    добавлен переменные окружения для модуля jwt

--- config/params.php
+++ config/params.php
@@ -4,4 +4,10 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+    'jwt' => [
+        'issuer' => $_ENV['JWT_ISSUER'],
+        'audience' => $_ENV['JWT_AUDIENCE'],
+        'id' => $_ENV['JWT_ID'],
+        'expire' => $_ENV['JWT_EXPIRE'],
+    ]
 ];
